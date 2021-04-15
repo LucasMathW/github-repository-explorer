f1fd1da4c45d3c0faecfe8f807b8305412be112f
LucasMathW <lucasag0408@outlook.com>
2021-04-15 15:45:08 -0400

    styling forms and repository divs

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import LogoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={LogoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite aqui" type="text" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/51097168?v=4"
+            alt="Lucas Matheus"
+          />
+          <div>
+            <strong>lucasMathW/project-manager</strong>
+            <p>
+              Aplicação desenvolvida para avalição de bolsa de estudos do IFAM
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/51097168?v=4"
+            alt="Lucas Matheus"
+          />
+          <div>
+            <strong>lucasMathW/project-manager</strong>
+            <p>
+              Aplicação desenvolvida para avalição de bolsa de estudos do IFAM
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars.githubusercontent.com/u/51097168?v=4"
+            alt="Lucas Matheus"
+          />
+          <div>
+            <strong>lucasMathW/project-manager</strong>
+            <p>
+              Aplicação desenvolvida para avalição de bolsa de estudos do IFAM
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 import background from '../assets/background.svg';
 
 export default createGlobalStyle`
-  *{  padding: 0;
+  *{  
+      padding: 0;
       margin: 0;
       outline: 0;
       box-sizing: border-box;
